c25027a83206defed02bccd79dcf6edb90a0e8ff
David Garcia <gtdavid2013@gmail.com>
2024-10-23 14:56:56 -0600

    Anadir funciones de suma y resta

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,10 @@
 num1 = int(input("Escribe un numero bro:"))
 num2 = int(input("Escribe otro numero bro:"))
+
+op = input("Escribe un + o - para hacer la operacion")
+if op == '-':
+    res = num1-num2
+elif op == '+':
+    res = num1-num2
+
+print(f"Tu Resultado fue: {res}")
